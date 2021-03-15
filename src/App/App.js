c97ac456507f5ecedf27485ab1ab6373ab7160f8
Brooke Nemchak <bnemchak@gmail.com>
2021-03-14 19:33:21 -0500

    basic setup

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AuthHome from '../AuthHome/AuthHome';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <AuthHome />
+    );
+  }
+}
+
+export default App;
