7197bb4406d4335ca731851f6b4e5827a7a2f960
Brooke Nemchak <bnemchak@gmail.com>
2021-03-25 14:32:15 -0500

    WIP

--- src/components/pages/Tasks/Tasks.js
+++ src/components/pages/Tasks/Tasks.js
@@ -10,6 +10,11 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Favorite from '@material-ui/icons/Favorite';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import Edit from '@material-ui/icons/Edit';
+import Modal from '@material-ui/core/Modal';
+import TextField from '@material-ui/core/TextField';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import Title from '../LandingPage/Title';
 
 import TaskData from '../../../helpers/data/taskData';
@@ -18,26 +23,97 @@ function preventDefault(event) {
   event.preventDefault();
 }
 
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
 const useStyles = makeStyles((theme) => ({
   seeMore: {
     marginTop: theme.spacing(3),
+    paper: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      padding: theme.spacing(2, 4, 3),
+    },
   },
 }));
 
 export default function Orders() {
   const classes = useStyles();
   const [tasks, setTasks] = useState([]);
+  const [taskId, setTaskId] = useState(null);
   const getTaskData = () => {
     TaskData.getTaskByID()
       .then((res) => {
         console.log(res);
-        setTasks(res.data.results);
+        setTasks(res.data);
       })
       .catch((err) => console.error(err));
   };
   useEffect(() => {
     getTaskData();
   }, []);
+
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = (e) => {
+    setOpen(true);
+    console.log(e);
+    // this.setTaskId
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const taskUpdate = (e) => {
+    e.preventDefault();
+    this.setState({ label: e.target.value });
+  };
+
+  const submitTask = (e) => {
+    e.preventDefault();
+    const { label, updating, Id } = this.state;
+    const task = { label };
+    const jsonTask = JSON.stringify(task);
+
+    if (updating) {
+      TaskData.updateTask(jsonTask, Id)
+        .then(() => {
+          this.setState({ isOpen: false, label: '' });
+          this.getTaskData();
+        })
+        .catch((err) => console.error(err));
+    } else {
+      TaskData.addTask(jsonTask)
+        .then(() => {
+          this.setState({ isOpen: false, label: '' });
+          this.getTaskData();
+        })
+        .catch((err) => console.error(err));
+    }
+  };
+
+  const deleteTask = (id) => {
+    TaskData.deleteTask(id)
+      .then(() => { this.getTaskData(); })
+      .catch((err) => console.error(err));
+  };
   return (
     <React.Fragment>
       <Title>Tasks</Title>
@@ -48,7 +124,10 @@ export default function Orders() {
               <Link href="/{row.name}" onClick={preventDefault}>
               <TableCell>
               <FormControlLabel control={ <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} name="checkedH" />} label={row.name}/>
+              <HighlightOffIcon name="delete" onClick={deleteTask} />
+              <Edit name="edit" onClick={handleOpen} id={row.id} />
               </TableCell>
+              <TableCell><TextField id="standard-basic" label="Add" onChange={submitTask}/></TableCell>
               </Link>
             </TableRow>
           ))}
@@ -60,6 +139,19 @@ export default function Orders() {
           Hold That Thought
         </Link>
       </div>
+      <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="standard-basic" label="Edit" onChange={taskUpdate}/>
+        <CheckCircleIcon name="submit" onClick={submitTask} />
+      </form>
+      </Modal>
+    </div>
     </React.Fragment>
   );
 }
--- src/helpers/data/taskData.js
+++ src/helpers/data/taskData.js
@@ -2,6 +2,14 @@ import axios from 'axios';
 
 const url = 'http://127.0.0.1:8000';
 
+const headers = () => (
+  {
+    headers: {
+      Authorization: `Token ${localStorage.getItem('token')}`,
+    },
+  }
+);
+
 const createHeaders = {
   headers: {
     Authorization: `Token ${localStorage.getItem('token')}`,
@@ -11,4 +19,13 @@ const createHeaders = {
 
 const getTaskByID = (id) => axios.get('http://localhost:8000/tasks', createHeaders);
 
-export default { getTaskByID };
+const updateTasks = (obj, id) => axios.put(`http://localhost:8000/tasks/${id}`, obj, createHeaders);
+
+const deleteTasks = (id) => axios.delete(`http://localhost:8000/tasks/${id}`, headers());
+
+const createTasks = (newArticle) => axios.post('http://localhost:8000/tasks', newArticle, createHeaders);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  getTaskByID, updateTasks, deleteTasks, createTasks,
+};
